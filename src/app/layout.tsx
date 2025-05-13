b01512f6eadf7d73d824f06d1bd6ff64dcb1d5d7
Tyaan Singh <singhtyaan@gmail.com>
2025-05-13 16:08:52 +1200

    adds rainbow favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Color Sequence Game",
   description: "Fun memory game! Try to recall color sequences.",
+  icons:{
+    icon: "/icons8-rainbow-16.png", 
+  }
 };
 
 export default function RootLayout({
